b92db5cc8a13017b2d2f6994850cac149f8117db
Wael Mohamed BETTAYEB <bettayebwael@gmail.com>
2020-08-20 15:49:05 +0100

    gather data

--- script.ts
+++ script.ts
@@ -3,9 +3,45 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  // Retrieve all published posts
-  const allUsers = await prisma.users.findMany({});
-  console.log(`Retrieved all published posts: `, allUsers);
+  // Retrieve all users
+  const allUsers = await prisma.users.findMany({
+    include: {
+      avatar: {
+        include: {
+          file: true,
+        },
+        where: {
+          related_type: "users-permissions_user",
+          order: 1,
+        },
+      },
+    },
+  });
+  console.log(`Retrieved all users: `, allUsers);
+
+  const allListing = await prisma.listings.findMany({
+    include: {
+      images: {
+        include: {
+          file: true,
+        },
+        where: {
+          related_type: "listings",
+          order: 1,
+        },
+      },
+    },
+    where: {
+      isPublished: true,
+    },
+  });
+  console.log(`Retrieved all listings: `, allListing);
+
+  // const allUsersMorph = await prisma.upload_file_morph.findMany({
+  //   where: { related_type: "users-permissions_user" },
+  //   include: { users: true },
+  // });
+  // console.log(`Retrieved all published posts: `, allUsersMorph);
 }
 
 main()
