e7736b8ca8a1e91a920077bd1b884eb42167dc6c
Wael Mohamed BETTAYEB <bettayebwael@gmail.com>
2020-08-20 17:27:54 +0100

    add location field

--- script.ts
+++ script.ts
@@ -1,30 +1,70 @@
 import { PrismaClient } from "@prisma/client";
+import MeiliSearch from "meilisearch";
 
 const prisma = new PrismaClient();
+const client = new MeiliSearch({ host: "http://127.0.0.1:7700" });
 
 async function main() {
   // Retrieve all users
   const allUsers = await prisma.users.findMany({
-    include: {
+    select: {
+      id: true,
+      email: true,
+      firstName: true,
+      lastName: true,
+      bio: true,
+      gender: true,
+      dateOfBirth: true,
       avatar: {
-        include: {
-          file: true,
-        },
         where: {
           related_type: "users-permissions_user",
           order: 1,
         },
+        select: {
+          file: {
+            select: {
+              url: true,
+            },
+          },
+        },
       },
+    },
+    where: {
+      confirmed: true,
     },
   });
   console.log(`Retrieved all users: `, allUsers);
 
   const allListing = await prisma.listings.findMany({
-    include: {
-      owner: true,
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      created_at: true,
+      updated_at: true,
+      listings_components: {
+        select: {
+          location: { select: { latitude: true, longitude: true } },
+        },
+        where: {
+          component_type: "components_location_geo_coordinates",
+        },
+      },
+      owner: {
+        select: {
+          id: true,
+          email: true,
+          firstName: true,
+          lastName: true,
+        },
+      },
       images: {
-        include: {
-          file: true,
+        select: {
+          file: {
+            select: {
+              url: true,
+            },
+          },
         },
         where: {
           related_type: "listings",
@@ -37,12 +77,28 @@ async function main() {
     },
   });
   console.log(`Retrieved all listings: `, allListing);
+  const peopleIndex = await client.getOrCreateIndex("people");
 
-  // const allUsersMorph = await prisma.upload_file_morph.findMany({
-  //   where: { related_type: "users-permissions_user" },
-  //   include: { users: true },
-  // });
-  // console.log(`Retrieved all published posts: `, allUsersMorph);
+  peopleIndex.addDocuments(allUsers as any);
+  peopleIndex.resetSearchableAttributes();
+  peopleIndex.updateSearchableAttributes([
+    "email",
+    "firstName",
+    "lastName",
+    "bio",
+    "gender",
+  ]);
+  const listingsIndex = await client.getOrCreateIndex("listings");
+
+  listingsIndex.addDocuments(allListing as any);
+  listingsIndex.resetSearchableAttributes();
+  listingsIndex.updateSearchableAttributes([
+    "title",
+    "description",
+    "owner",
+    "created_at",
+    "updated_at",
+  ]);
 }
 
 main()
